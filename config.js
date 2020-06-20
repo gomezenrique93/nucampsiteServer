7682a3066a61a5239d244affde237a7581797184
Enrique Gomez <gomez.enrique4595@gmail.com>
2020-06-20 13:55:06 -0700

    Last Day

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'secretKey': '12345-67890-09876-54321',
+  'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+  'facebook': {
+    clientID: '3642369412553099',
+    clientSecret: 'fa1184da3f215eaee24911fc0ef903d5'
+  }
+}
+
